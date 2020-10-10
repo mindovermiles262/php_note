11ba8eb98943371452347867c4524aee412285d7
sage <mindovermiles262@gmail.com>
2020-10-09 21:29:49 -0500

    annotated route

--- app/src/Controller/ArticleController.php
+++ app/src/Controller/ArticleController.php
@@ -2,8 +2,14 @@
   namespace App\Controller;
 
   use Symfony\Component\HttpFoundation\Response;
+  use Symfony\Component\Routing\Annotation\Route;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
   class ArticleController {
+    /**
+     * @Route("/")
+     * @Method({"GET"})
+     */
     public function index() {
         return new Response("
           <body>
