40225887923926aec439a4e8284b269aa07b7e46
sage <mindovermiles262@gmail.com>
2020-10-09 22:03:21 -0500

    use twig template, added bootstrap and navabar

--- app/src/Controller/ArticleController.php
+++ app/src/Controller/ArticleController.php
@@ -4,17 +4,23 @@
   use Symfony\Component\HttpFoundation\Response;
   use Symfony\Component\Routing\Annotation\Route;
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+  // Controller/Controller has been depricated in Symfony 5. Use
+  // AbstractController as a replacement
+  // https://stackoverflow.com/questions/59798041/class-controller-not-found-while-loading
+  use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-  class ArticleController {
+  # Article Controller will extend twig controller
+  class ArticleController extends AbstractController {
     /**
      * @Route("/")
      * @Method({"GET"})
      */
     public function index() {
-        return new Response("
-          <body>
-            <h1>Hello Symfony</h1>
-          </body>
-        ");
+      // return new Response("
+      //   <body>
+      //     <h1>Hello Symfony</h1>
+      //   </body>
+      // ");
+      return $this->render('articles/index.html.twig');
     }
   }
